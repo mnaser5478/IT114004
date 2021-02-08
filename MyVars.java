fa752562da35a3eb1df801d4786d7cd55d068c69
mnaser5478 <50526404+mnaser5478@users.noreply.github.com>
2021-02-08 18:27:34 -0500

    Add files via upload

---
+++ MyVars.java
@@ -0,0 +1,20 @@
+public class MyVars {
+	public static void main(String[] args) {
+		System.out.println("Java [IT114-004-S21] Exercise [#1]");
+		String carName = "Volvo";
+		System.out.println("Java [IT114-004-S21] Exercise [#2]");
+		int maxSpeed = 120;
+		System.out.println("Java [IT114-004-S21] Exercise [#3]");
+		int x = 5;
+		int y = 10;
+		System.out.println(x + y);
+		System.out.println("Java [IT114-004-S21] Exercise [#4]");
+		int x2 = 5;
+		int y2 = 10;
+		int z2 = x2 + y2;
+		System.out.println(z2);
+		System.out.println("Java [IT114-004-S21] Exercise [#5]");
+		int x3 = 5, y3 = 6, z3 = 50;
+		System.out.println(x3 + y3 + z3);
+	}
+}
