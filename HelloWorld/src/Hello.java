5732e8f1a9b8375f383429e2b3514c9ffd786173
mnaser5478 <mnaser5478@gmail.com>
2021-02-16 18:59:02 -0500

    Java Flow Control

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,10 +1,19 @@
 
-public class Hello {
+System.out.println("Java [IT114-004-S21] Exercise [#1]");
+public class MyClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		// TODO Auto-generated method stub
 
 	}
 
 }
+
+System.out.println("Java [IT114-004-S21] Exercise [#1]");
+// This is a single-line comment
+/* This 
+ * is 
+ * a 
+ * multi-line 
+ * comment
+ */
---
+++ IT114-004-S21 - [HW] - Java Vars/src/MySyntax.java
@@ -0,0 +1,13 @@
+public class MySyntax {
+	public static void main(String[] args) {
+		System.out.println("Java [IT114-004-S21] Exercise [#1]");
+		System.out.println("Hello World!");
+
+		System.out.println("Java [IT114-004-S21] Exercise [#2]");
+		// This is a single-line comment
+		/*
+		 * This is a multi-line comment
+		 */
+		
+	}
+}
